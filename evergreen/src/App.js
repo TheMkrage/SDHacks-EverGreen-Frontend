084001ffb58651ed098e2fcc82ac48ea5b6c7db3
alicechi2 <alicechi2@gmail.com>
2019-10-26 11:53:59 -0700

    add images and general framework for UI

--- evergreen/src/App.js
+++ evergreen/src/App.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import AceEditor from 'react-ace';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <AceEditor />
-    </div>
-  );
-}
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-dreamweaver";
 
-export default App;
+
+export default class App extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            EVERGREEN LMAOOOO
+          </p>
+        </header>
+        <div className="menu">
+          <p>
+          menu stuff change language idk
+          </p>
+        </div>
+        <div className="ide">
+          <AceEditor
+            mode="java"
+            theme="dreamweaver"
+            wrapEnabled="true"
+            height="80vh"
+            width="85vw"
+            className="ide-editor"
+          />
+        </div>
+        <div className="scene">
+          <img src={require("../src/worst.png")} id="scene"/>
+        </div>
+      </div>
+    );
+  } 
+}
