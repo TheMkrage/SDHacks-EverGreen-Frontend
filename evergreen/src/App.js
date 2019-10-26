430dc3be4ef11c1fe0eaa9e7b0eb9d767dad9865
alicechi2 <alicechi2@gmail.com>
2019-10-26 00:19:14 -0700

    ide is in :)

--- evergreen/src/App.js
+++ evergreen/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import AceEditor from 'react-ace';
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AceEditor />
     </div>
   );
 }
