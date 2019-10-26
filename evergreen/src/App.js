90b64aa4a20d95c555387c55f06c9f6ac7c221f8
Matthew Krager <matthewkrager@gmail.com>
2019-10-26 12:41:42 -0700

    Make Request when user stops typing

--- evergreen/src/App.js
+++ evergreen/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AceEditor from 'react-ace';
+import { getPound } from './network/backend.js'
+import InputPanel from "./InputPanel.js"
 
 import './App.css';
 
@@ -11,6 +13,42 @@ export default class App extends React.Component {
 
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+        code: '',
+        typing: false,
+        typingTimeout: 0,
+        pounds: 0
+    }
+  }
+
+  onTypingEnded() {
+    console.log("typing ended!")
+    getPound(this.state.code, 100, "local")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        this.setState({ pounds: data.pounds })
+      })
+  }
+
+  onEditorChange(newValue) {
+
+    if (this.state.typingTimeout) {
+       clearTimeout(this.state.typingTimeout);
+    }
+
+    console.log(newValue)
+    const self = this
+    this.setState({
+       code: newValue,
+       typing: false,
+       typingTimeout: setTimeout(function () {
+           self.onTypingEnded();
+         }, 2000)
+    });
   }
 
   render(){
@@ -23,23 +61,27 @@ export default class App extends React.Component {
         </header>
         <div className="menu">
           <p>
-          menu stuff change language idk
+            menu stuff change language idk
+             Pounds: {this.state.pounds}
           </p>
         </div>
         <div className="ide">
           <AceEditor
             mode="java"
+            value={this.state.code}
             theme="dreamweaver"
             wrapEnabled="true"
             height="80vh"
             width="85vw"
+            onChange={this.onEditorChange.bind(this)}
             className="ide-editor"
           />
+          <InputPanel />
         </div>
         <div className="scene">
           <img src={require("../src/worst.png")} id="scene"/>
         </div>
       </div>
     );
-  } 
+  }
 }
---
+++ evergreen/src/InputPanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class InputPanel extends React.Component {
+  render() {
+    return (
+      <div>
+        InputPanel
+      </div>
+    )
+  }
+}
